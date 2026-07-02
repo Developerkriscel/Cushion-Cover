7de14d777df4f50332eb2ab0d855c15a67a034f8
Tannu067 <tannu067btece22@igdtuw.ac.in>
2026-07-02 13:16:33 +0530

    force IPv4 for gmail smtp to fix ENETUNREACH error

--- server/utils/email.js
+++ server/utils/email.js
@@ -8,6 +8,7 @@ const getGmailTransporter = () => {
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
